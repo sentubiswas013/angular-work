3870a9491880244da327d6c8ae17cf0a2b7a8a3d
sentubiswas013 <sentu.biswas13@gmail.com>
2024-11-14 00:58:30 +0530

    test

---
+++ input-form/input-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-input-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './input-form.component.html',
+  styleUrl: './input-form.component.css',
+})
+export class InputFormComponent {}
